4363bbaf99f6be4d873bab95af684c1266d2ecc1
Yeobi <yeobi1124@gmail.com>
2025-01-26 01:03:04 +0900

    feat : Develop backend api except test
    백엔드 대략 개발. 테스트는 아직 하지 않아 확인이 필요함.
    
    Type: Title
    
    (Body)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,35 @@
 import express from 'express';
-import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
 import { db } from './db.js';
 
-const app = express();
-const port = 3001;
+// 임시로 갔다 놓은거 나중에 구조 바꿔야징
+dotenv.config();
+const { DB_NAME } = process.env;
+// 
 
-// 데이터 가져오기 API
-app.get('/data', (req, res) => {
-  const query = 'SELECT * FROM your_table_name'; // 원하는 쿼리 작성
+const { PORT } = process.env;
+
+const app = express();
+app.use(express.json());
+
+app.get('/balance', (req, res) => {
+  const { userid, serverid } = req.body;
+  const response = { userid, balance };
+
+  const query = 'SELECT balance FROM ${DB_NAME} WHERE userid = ${userid} AND serverid = ${serverid}';
   db.query(query, (err, results) => {
     if (err) {
       console.error('데이터 조회 오류:', err);
       res.status(500).send('서버 오류');
     } else {
-      res.json(results); // 데이터를 JSON 형태로 반환
+      response.userid = userid;
+      response.balance = results[0].balance;
+      res.json(response);
     }
   });
 });
 
-// 서버 시작
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
 });
