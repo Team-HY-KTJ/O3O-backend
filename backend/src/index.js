9f0b891930b20f088e43aea7e8deb04513a06d3e
Yeobi <yeobi1124@gmail.com>
2025-01-26 00:31:04 +0900

    feat : develop MySQL DB connection
    db.js에서 MySQL이랑 연결 만들고, 연결을 export해서 사용할 수 있도록
    만듦.
    
    Type: Title
    
    (Body)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,60 +1,10 @@
-const express = require('express');
-const mysql = require('mysql2');
+import express from 'express';
+import mysql from 'mysql2';
+
+import { db } from './db.js';
 
 const app = express();
-const port = 3000;
-
-// MySQL 데이터베이스 연결 설정
-const db = mysql.createConnection({
-  host: 'localhost', // MySQL 서버 주소
-  user: 'root',      // MySQL 사용자 이름 (root는 모든 권한을 가진 사용자)
-  password: '*********', // MySQL 비밀번호
-  database: 'test',   // 사용할 데이터베이스 이름
-  port: 3306 // 사용 포트 (MySQL의 default port가 3306)
-});
-
-// MySQL 연결
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL 연결 실패:', err);
-  } else {
-    console.log('MySQL 연결 성공');
-
-    const query = 'SELECT * FROM table1';
-    db.query(query, (err, results) => {
-        if (err) {
-          console.error('데이터 조회 오류:', err);
-        } else {
-            console.log("데이터 조회 성공");
-            console.log(results);
-        }
-      });
-  }
-});
-
-/*
-서버가 http://localhost:3000에서 실행 중입니다.
-MySQL 연결 성공
-데이터 조회 성공
-[
-  {
-    id: 1,
-    title: 'MySQL',
-    description: 'MySQL is ...',
-    created: 2025-01-25T10:45:40.000Z,
-    author: 'egoing',
-    profile: 'developer'
-  },
-  {
-    id: 2,
-    title: 'TestData',
-    description: 'Test description',
-    created: 2025-01-25T11:20:32.000Z,
-    author: 'Yeobi',
-    profile: 'Test profile'
-  }
-]
-*/
+const port = 3001;
 
 // 데이터 가져오기 API
 app.get('/data', (req, res) => {
