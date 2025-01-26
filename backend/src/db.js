07c81bdead04d7caaaa013da410db5caffbd91c5
Yeobi <yeobi1124@gmail.com>
2025-01-26 17:22:41 +0900

    feat : eslint 적용
    - .eslintrc 파일 사용 버전이 8버전이라서 eslint@8.57.0을 install 함.
    - 사용을 위해서는 ESLint Extension 설치가 필요하고, setting.json에서 다음
    내용의 추가가 필요함.
    
    ```
    "eslint.validate": ["javascript"],
    "editor.codeActionsOnSave": {
        "source.fixAll.eslint": "explicit",
    }
    ```
    
    - "editor.formatOnSave": true, 도 써봤는데 얘는 뭔가 충돌이 발생하는지
    규칙에 안 맞게 다시 되돌려 버려서 뺌.
    
    Type: Title
    
    (Body)

--- backend/src/db.js
+++ backend/src/db.js
@@ -6,19 +6,19 @@ dotenv.config();
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const db = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  port: DB_PORT,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+    host: DB_HOST,
+    user: DB_USER,
+    port: DB_PORT,
+    password: DB_PASSWORD,
+    database: DB_NAME,
 });
 
 db.connect((err) => {
     if (err) {
-      console.error('MySQL DB Connect Failed:', err);
+        console.error('MySQL DB Connect Failed:', err);
     } else {
-      console.log('MySQL DB Connect Success');
+        console.log('MySQL DB Connect Success');
     }
 });
 
-export { db };
+export default { db };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import { db } from './db.js';
+import db from './db';
 
 // 임시로 갔다 놓은거 나중에 구조 바꿔야징
 dotenv.config();
 const { DB_TABLENAME } = process.env;
-// 
+// end
 
 const { PORT } = process.env;
 
@@ -14,26 +14,26 @@ const app = express();
 app.use(express.json());
 
 app.get('/balance', (req, res) => {
-  const { userid, serverid } = req.query;
-  const response = { userid:0, balance:0 };
+    const { userid, serverid } = req.query;
+    const response = { userid: 0, balance: 0 };
 
-  console.log(`userid: ${userid}, serverid: ${serverid}`);
-  
-  const query = `SELECT user_id, balance FROM ${DB_TABLENAME} WHERE user_id = ${userid} AND server_id = ${serverid}`;
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('데이터 조회 오류:', err);
-      res.status(500).send('서버 오류');
-    } else {
-      console.log("전송 선공");
-      response.userid = results[0].userid;
-      response.balance = results[0].balance;
-      console.log(response);
-      res.json(response);
-    }
-  });
+    console.log(`userid: ${userid}, serverid: ${serverid}`);
+
+    const query = `SELECT user_id, balance FROM ${DB_TABLENAME} WHERE user_id = ${userid} AND server_id = ${serverid}`;
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('데이터 조회 오류:', err);
+            res.status(500).send('서버 오류');
+        } else {
+            console.log('전송 선공');
+            response.userid = results[0].userid;
+            response.balance = results[0].balance;
+            console.log(response);
+            res.json(response);
+        }
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
+    console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
 });
