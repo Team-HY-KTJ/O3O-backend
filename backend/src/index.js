ba2d23425cfde90a2345f305f2b200a33d1e15a9
Yeobi <yeobi1124@gmail.com>
2025-01-26 15:18:17 +0900

    feat : Develop backend api
    저번 커밋에서 MySQL DB는 container로 분리, backend와 frontend 부분은
    로컬 환경에서 실행해서 테스트. 작동하도록 수정함.
    
    작동함에 있어 .env 파일에 다음 내용이 필요함.
    DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_TABLENAME, PORT
    DB 붙은 건 mysql 조금 해보면 알 수 있는 내용이고, PORT는 백엔드에서
    요청받을 포트
    
    Type: Title
    
    (Body)

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import { db } from './db.js';
 
 // 임시로 갔다 놓은거 나중에 구조 바꿔야징
 dotenv.config();
-const { DB_NAME } = process.env;
+const { DB_TABLENAME } = process.env;
 // 
 
 const { PORT } = process.env;
@@ -14,22 +14,26 @@ const app = express();
 app.use(express.json());
 
 app.get('/balance', (req, res) => {
-  const { userid, serverid } = req.body;
-  const response = { userid, balance };
+  const { userid, serverid } = req.query;
+  const response = { userid:0, balance:0 };
 
-  const query = 'SELECT balance FROM ${DB_NAME} WHERE userid = ${userid} AND serverid = ${serverid}';
+  console.log(`userid: ${userid}, serverid: ${serverid}`);
+  
+  const query = `SELECT userid, balance FROM ${DB_TABLENAME} WHERE userid = ${userid} AND serverid = ${serverid}`;
   db.query(query, (err, results) => {
     if (err) {
       console.error('데이터 조회 오류:', err);
       res.status(500).send('서버 오류');
     } else {
-      response.userid = userid;
+      console.log("전송 선공");
+      response.userid = results[0].userid;
       response.balance = results[0].balance;
+      console.log(response);
       res.json(response);
     }
   });
 });
 
-app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
 });
