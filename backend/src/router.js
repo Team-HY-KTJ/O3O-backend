b1f1b8c5d093fbebad8f0455e94013620051f9ac
Yeobi <yeobi1124@gmail.com>
2025-01-27 15:35:26 +0900

    feat : 파일 자동 import
    - 기존꺼에서 동적 import가 잘 안돼서 하드코딩으로 남겨뒀는데 자동으로
      가져오게 해결함.
    
    Type: Title
    
    (Body)

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,29 +8,26 @@ const router = express.Router();
 const routerRootPath = './src/routerRoot';
 
 async function GetRouters() {
-    console.log("GetRoute start");
     const dirs = fs.readdirSync(routerRootPath, { withFileTypes: true });
 
     for (const dir of dirs) {
-        console.log("Read");
-
         if (!dir.isDirectory()) return;
-
-        console.log(`Dir: ${path.join(process.cwd(), routerRootPath, dir.name)}`);
 
         const files = fs.readdirSync(path.join(process.cwd(), routerRootPath, dir.name));
 
         for (const file of files.filter((file) => file.endsWith('.js'))) {
-            console.log(`file: ${path.join(process.cwd(), routerRootPath, dir.name, file)}`);
-            const hardcoding = './routerRoot/balance/GETbalance.js';
-            console.log(hardcoding);
-            const { route: moduleRoute, router: moduleRouter } = await import(hardcoding);
+            const { route: moduleRoute, router: moduleRouter } = await import('../' + path.join(routerRootPath, dir.name, file));
 
             router.use(moduleRoute, moduleRouter);
         }
     }
+}
 
-    /*
+await GetRouters();
+
+export { route, router };
+
+/* fs.readdir로 비동기 작업 시도 (나중에 비동기로 작업할 때를 위해 남겨둠.)
     dirs.forEach((dir) => {
         console.log("Read");
 
@@ -63,9 +60,3 @@ async function GetRouters() {
         
     });
     */
-    console.log("Get Router Success");
-}
-
-GetRouters();
-
-export { route, router };
