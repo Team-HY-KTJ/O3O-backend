3801a3b95cfe77d8d55bc644dd4627b15c3c11a1
kwon young jin <kwon120@hanyang.ac.kr>
2025-01-31 23:18:22 +0900

    feat: ranking 함수 제작
    (ranking함수에 대한 이해를 바탕으로 원하는 객체를 반환하도록 제작)

--- backend/src/routerRoot/balance/ranking.js
+++ backend/src/routerRoot/balance/ranking.js
@@ -1,33 +1,50 @@
-const mysql = require('mysql2');
-const serverID = require('./bot');
+import express, { response } from 'express';
+import db from '../../db.js';
 
-const db = mysql.createConnection({
-  host: 'localhost', // MySQL 서버 주소
-  user: 'root',      // MySQL 사용자 이름 (root는 모든 권한을 가진 사용자)
-  password: '*********', // MySQL 비밀번호
-  database: 'test',   // 사용할 데이터베이스 이름
-  port: 3306 // 사용 포트 (MySQL의 default port가 3306)
-});
+const route = 'balance';
+const router = express.Router();
 
-export async function ranking(input){
-    try{
-        // 상위 5명의 id와 코인 수만 fiveguys에 입력
-        const [fiveguys] = await db.execute('SELECT user_id, coins FROM users ORDER BY coins DESC LIMIT 5');
-        // 코인을 보유한 5명(이하)에 해당하는 메시지 반환
-        if(fiveguys.length > 0){
-            let message = `상위 ${fiveguys.length}명의 보유 코인량\n`;
-            for(let i=0; i<fiveguys.length; i++){
-                message += `\n${i+1}위 | ${fiveguys[i].user_id} | ${fiveguys[i].coins}`;
-            }
-            await input.reply(message);
-        }
-        // 코인을 보유한 인원이 1명도 없을 때에 메시지 반환환
-        else {
-            await input.reply('여긴 빈털터리밖에 없네');
+router.get('/ranking', (req, res) => {
+    const { guildId, scope='top', limit=10, page=1 } = req.query();
+    // limit가 100보다 큰 경우
+    if(limit > 100) return res.status(422).json({ error: 'Too much request(Reduce the limit)'});
+    // 서버 인원 구하기
+    const [count] = db.execute('SELECT COUNT(*) AS count FROM users WHERE server_id = ?',[guildId]);
+    if(scope==='top'){
+        // user_id, coins 구하기
+        const [ranking] = db.execute('SELECT user_id, coins FROM users WHERE server_id = ? ORDER BY coins DESC LIMIT ?',[guildId, limit]);
+        // 반환 객체 생성성
+        const response = {
+            guildId: guildId,
+            ranking: ranking.map(rank => ({
+                userId: rank.user_id,
+                balance: rank.coins
+            })),
+            totalCount: count[0].count
         }
     }
-    catch{
-        console.log('/ranking error');
-        await input.reply('피곤해 자러갈래');
+    else if(scope==='all'){
+        // user_id, coins 구하기
+        const [ranking] = db.execute('SELECT user_id, coins FROM users WHERE server_id = ? ORDER BY coins DESC LIMIT ? OFFSET ?',[guildId, limit, limit*(page-1)]);
+        // 반환 객체 생성
+        const response = {
+            guildId: guildId,
+            ranking: ranking.map(rank => ({
+                userId: rank.user_id,
+                balance: rank.coins
+            })),
+            page: page,
+            totalPage: Math.ceil(count[0].count/page),
+            limit: limit,
+            totalCount: count[0].count
+        }
     }
-}
+    // 잘못된 scope를 입력했을 때
+    else{
+        return res.status(400).json({ error: 'Wrong scope' });
+    }
+    // JSON 반환
+    res.json(response);
+})
+
+export { route, router };
